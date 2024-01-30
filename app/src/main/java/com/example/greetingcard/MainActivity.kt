285195a542e987141310e82f0933ecf740f79619
Ethan Sterk <ethansterk@gmail.com>
2024-01-30 08:40:31 -0500

    Greeting Card Project

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.greetingcard
+
+import android.app.AppOpsManager
+import android.app.usage.UsageStatsManager
+import android.os.Bundle
+import android.os.Process
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("onCreate()")
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Ethan")
+
+
+                }
+            }
+        }
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop()")
+    }
+
+    override fun onDestroy() {
+        println("onDestroy()")
+        super.onDestroy()
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Android")
+    }
+}
